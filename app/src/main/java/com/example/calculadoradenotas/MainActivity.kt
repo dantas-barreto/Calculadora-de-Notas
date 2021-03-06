7e66570e51ccda128bc2c33862da85318f81647a
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2021-03-06 17:38:29 -0300

    commit
    Entendendo como criar um projeto Android do zero

---
+++ app/src/main/java/com/example/calculadoradenotas/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.calculadoradenotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btCalcular = calcular
+        val resultado = resultado
+
+        btCalcular.setOnClickListener { it
+
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media = (nota1 + nota2) / 2
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            if (media >= 6 && faltas <= 10) {
+                resultado.setText("Aluno Aprovado" + "\n" + "Nota Final: $media" + "\n" + "Faltas: $faltas")
+            }
+            else {
+                resultado.setText("Aluno Reprovado" + "\n" + "Nota Final: $media" + "\n" + "Faltas: $faltas")
+            }
+        }
+    }
+}
